c17a6a162e8085cb15fb4c8e6c0921a677eb8f79
jeebiz <hnxyhcwdl1003@163.com>
2018-05-19 17:00:59 +0800

    完善 CtAnnotationBuilder

--- src/main/java/com/github/vindell/javassist/bytecode/CtAnnotationBuilder.java
+++ src/main/java/com/github/vindell/javassist/bytecode/CtAnnotationBuilder.java
@@ -44,9 +44,18 @@ import javassist.CtClass;
 import javassist.CtField;
 import javassist.bytecode.ConstPool;
 import javassist.bytecode.annotation.Annotation;
+import javassist.bytecode.annotation.AnnotationMemberValue;
 import javassist.bytecode.annotation.ArrayMemberValue;
+import javassist.bytecode.annotation.BooleanMemberValue;
+import javassist.bytecode.annotation.ByteMemberValue;
+import javassist.bytecode.annotation.CharMemberValue;
 import javassist.bytecode.annotation.ClassMemberValue;
+import javassist.bytecode.annotation.DoubleMemberValue;
 import javassist.bytecode.annotation.EnumMemberValue;
+import javassist.bytecode.annotation.FloatMemberValue;
+import javassist.bytecode.annotation.IntegerMemberValue;
+import javassist.bytecode.annotation.LongMemberValue;
+import javassist.bytecode.annotation.ShortMemberValue;
 import javassist.bytecode.annotation.StringMemberValue;
 
 /**
@@ -86,8 +95,45 @@ public class CtAnnotationBuilder implements Builder<Annotation> {
 		this.constPool = constPool;
 	}
 
-	public CtAnnotationBuilder addStringMember(String name, String value) {
-		annotation.addMemberValue(name, new StringMemberValue(value, constPool));
+	public CtAnnotationBuilder addAnnotationMember(String name, Annotation value) {
+		annotation.addMemberValue(name, new AnnotationMemberValue(value, constPool));
+		return this;
+	}
+	
+	public CtAnnotationBuilder addArrayMember(String name, String[] values) {
+		ArrayMemberValue member = new ArrayMemberValue(constPool);
+		StringMemberValue[] members = new StringMemberValue[values.length];
+		for (int i = 0; i < values.length; i++) {
+			members[i] = new StringMemberValue(values[i], constPool);
+		}
+		member.setValue(members);
+		annotation.addMemberValue(name, member);
+		return this;
+	}
+	
+	public CtAnnotationBuilder addBooleanMember(String name, boolean value) {
+		annotation.addMemberValue(name, new BooleanMemberValue(value, constPool));
+		return this;
+	}
+	
+	public CtAnnotationBuilder addByteMember(String name, byte value) {
+		annotation.addMemberValue(name, new ByteMemberValue(value, constPool));
+		return this;
+	}
+	
+	public CtAnnotationBuilder addCharMember(String name, char value) {
+		annotation.addMemberValue(name, new CharMemberValue(value, constPool));
+		return this;
+	}
+
+	public CtAnnotationBuilder addClassMember(String name, String value) {
+		ClassMemberValue classValue = new ClassMemberValue(value, constPool);
+		annotation.addMemberValue(name, classValue);
+		return this;
+	}
+	
+	public CtAnnotationBuilder addDoubleMember(String name, double value) {
+		annotation.addMemberValue(name, new DoubleMemberValue(value, constPool));
 		return this;
 	}
 
@@ -100,20 +146,28 @@ public class CtAnnotationBuilder implements Builder<Annotation> {
 		return this;
 	}
 
-	public CtAnnotationBuilder addClassMember(String name, String value) {
-		ClassMemberValue classValue = new ClassMemberValue(value, constPool);
-		annotation.addMemberValue(name, classValue);
+	public CtAnnotationBuilder addFloatMember(String name, float value) {
+		annotation.addMemberValue(name, new FloatMemberValue(value, constPool));
+		return this;
+	}
+	
+	public CtAnnotationBuilder addIntegerMember(String name, int value) {
+		annotation.addMemberValue(name, new IntegerMemberValue(constPool, value));
+		return this;
+	}
+	
+	public CtAnnotationBuilder addLongMember(String name, long value) {
+		annotation.addMemberValue(name, new LongMemberValue(value, constPool));
 		return this;
 	}
 
-	public CtAnnotationBuilder addArrayMember(String name, String[] values) {
-		ArrayMemberValue member = new ArrayMemberValue(constPool);
-		StringMemberValue[] members = new StringMemberValue[values.length];
-		for (int i = 0; i < values.length; i++) {
-			members[i] = new StringMemberValue(values[i], constPool);
-		}
-		member.setValue(members);
-		annotation.addMemberValue(name, member);
+	public CtAnnotationBuilder addShortMember(String name, short value) {
+		annotation.addMemberValue(name, new ShortMemberValue(value, constPool));
+		return this;
+	}
+	
+	public CtAnnotationBuilder addStringMember(String name, String value) {
+		annotation.addMemberValue(name, new StringMemberValue(value, constPool));
 		return this;
 	}
 
